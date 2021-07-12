da44c34c7061816a22ba50673049ac32dbef0ed3
Anton Chyvikov <chyvikov.anton@nixsolutions.com>
2021-07-12 16:38:48 +0300

    fix: change view of 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-css/semantic.css";
 
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container, Header} from "semantic-ui-react";
 
 import { HeaderComponent } from "./components/HeaderComponent";
 import { ProductList } from "./components/ProductList";
@@ -15,7 +16,14 @@ function App() {
         <Switch>
           <Route path='/' exact component={ProductList} />
           <Route path='/product/:productId' exact component={ProductDetail} />
-          <Route>404 Not Found</Route>
+          <Route>
+            <Container>
+              {" "}
+              <Header as='h1'>
+                404 Not Found
+              </Header>
+            </Container>
+          </Route>
         </Switch>
       </Router>
     </div>
